5f27d5494f90c15fa3f776dc56719cbefb7e654c
Henrik Nilsson <henrik.nilsson@tvaaker.se>
2017-02-27 16:23:38 +0100

    Skip node_modules, tmp, dist directories for ctags

--- .emacs.d/elisp/hn-compile.el
+++ .emacs.d/elisp/hn-compile.el
@@ -110,7 +110,7 @@
 		  )
 		(setq old-path default-directory)
 		(setq default-directory (hn-find-vc-root))
-		(shell-command "ctags -e --recurse=yes --exclude=mootools*.js --exclude=dygraph*.js --exclude=jx*.js --exclude=*.css --exclude=venv")
+		(shell-command "ctags -e --recurse=yes --exclude=mootools*.js --exclude=dygraph*.js --exclude=jx*.js --exclude=*.css --exclude=venv --exclude=node_modules --exclude=dist --exclude=tmp")
 		(visit-tags-table (concat default-directory "/TAGS"))
 		(setq default-directory old-path)
 		)
